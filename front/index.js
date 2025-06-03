437aaff90b4083923c645f4a14d696a2b021a75d
Akhrasmiy <akhrasmiy@gmail.com>
2025-06-03 18:18:05 +0500

    same"

--- front/index.js
+++ front/index.js
@@ -3,7 +3,7 @@ import path from "path";
 import { fileURLToPath } from "url";
 
 const app = express();
-const PORT = process.env.PORT || 1893;
+const PORT = process.env.PORT || 9003;
 
 // __dirname'ni to'g'ri aniqlash
 const __filename = fileURLToPath(import.meta.url);
@@ -12,12 +12,14 @@ const __dirname = path.dirname(__filename);
 // Statik fayllarni xizmat qilish
 app.use(express.static(path.join(__dirname, "dist")));
 
+
 // Barcha boshqa yo'llar uchun index.html'ni yuborish
 app.get("/*", (req, res) => {
   res.sendFile(path.join(__dirname, "dist", "index.html"));
 });
 
 // Serverni tinglash
-app.listen(PORT, () => {
+const server=app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
+server.timeout = 4500000;
